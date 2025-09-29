2676cb811bb6ea08c7c56f6f96d4a7f09401694b
Vergazon <herbemalveillante@gmail.com>
2025-09-29 16:13:27 +0200

    blah

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ As this project uses **Git Submodules** to load the Axle Engine, you should clon
 ```bash
 git clone --recurse-submodules https://github.com/HerbeMalveillante/AxleExamples.git
 ```
+
+## Logisim
+
+### GUI
+1. We need to be able to move around a grid, zoom and unzoom using the trackpad.
+
+2. We need to manage a complex UI layout with buttons, panels, and multiple controls
+
+3. We need to manage a real-time gate simulation.
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,67 @@
 #include "raylib.h"
+#include "raymath.h"
+#include "rlgl.h"
 
 int main() {
-  SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
-  InitWindow(800, 600, "Axle Engine");
+  SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI | FLAG_WINDOW_RESIZABLE);
+  InitWindow(900, 600, "Axle Engine");
   SetTargetFPS(60);
 
+  Camera2D camera = {{0}};
+  camera.zoom = 1.0f;
+
   while (!WindowShouldClose()) {
+    // Update
+    // ============================================================
+
+    // Translate based on mouse right click
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+      Vector2 delta = GetMouseDelta();
+      delta = Vector2Scale(delta, -1.0f / camera.zoom);
+      camera.target = Vector2Add(camera.target, delta);
+    }
+
+    // Zoom based on mouse wheel
+    float wheel = GetMouseWheelMove();
+    if (wheel != 0) {
+      // Get the world point under the mouse
+      Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+
+      // Set the offset to where the mouse is
+      camera.offset = GetMousePosition();
+
+      // Set the target to match, so that the camera maps the world space point
+      // under the cursor to the screen space point under the cursor at any zoom
+      camera.target = mouseWorldPos;
+
+      // Zoom increment
+      // Uses log scaling to provide consistent zoom speed
+      float scale = 0.2f * wheel;
+      camera.zoom = Clamp(expf(logf(camera.zoom) + scale), 0.125f, 64.0f);
+    }
+
+    // Draw
+    // ============================================================
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawText("Welcome to Axle Engine!", 10, 10, 30, GRAY);
+
+    BeginMode2D(camera);
+    // Draw the 3D grid, rotated 90 degrees and centered around 0,0 just so we have something in the XY plane.
+    // This can be replaced with a shader later on, to render the grid.
+    rlPushMatrix();
+        rlTranslatef(0, 25*50, 0);
+        rlRotatef(90, 1, 0, 0);
+        DrawGrid(100, 50);
+    rlPopMatrix();
+
+    // Draw a reference circle
+    DrawCircle(GetScreenWidth()/2, GetScreenHeight()/2, 50, MAROON);
+    EndMode2D();
+
+    // Draw mouse reference
+    DrawCircleV(GetMousePosition(), 4, DARKGRAY);
+    DrawTextEx(GetFontDefault(), TextFormat("[%i, %i]", GetMouseX(), GetMouseY()),
+        Vector2Add(GetMousePosition(), (Vector2){-44, -23 }), 20, 2, BLACK);
     EndDrawing();
   }
 
